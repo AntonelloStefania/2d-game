7e118a45c64eb034c6c715c7728b01c95bfc0ca1
AntonelloStefania <antonelloste94@gmail.com>
2023-10-05 21:34:13 +0200

    sistemata tolleranza caduta buchi

--- src/components/Game.vue
+++ src/components/Game.vue
@@ -10,6 +10,9 @@ export default {
       obstacleWidth:50,
       obstacleHeigth:80,
       obstacles:[],
+      holes:[],
+      holeWidth:40,
+      holeHeigth:200,
       maxObstacles: 4,
       playerWidth:100,
       gameOver:false,
@@ -94,8 +97,10 @@ export default {
   
       var maxPositionX = window.innerWidth;
       var NumberObstacles =  this.levels.find(level => level.level === this.currentLevel).maxObstacles;
+      var NumberHoles =  this.levels.find(level => level.level === this.currentLevel).maxHoles;
       const safeZoneWidth = 180;
      
+      //OSTACOLI
       var obstacleWrapper= document.querySelector('.obstacle-wrapper');
       obstacleWrapper.innerHTML=''
       for(let i=1; i<=NumberObstacles; i++){
@@ -110,6 +115,36 @@ export default {
       obstacleWrapper.appendChild(obstacle);
       }
 
+      //BUCHI
+      
+      const visualHoleWidth = 130; // Larghezza visiva del buco (compresi i bordi di 10px da entrambi i lati)
+      const tolerance = (visualHoleWidth - this.holeWidth) / 2; // Tolleranza equamente distribuita su entrambi i lati
+      var holeWrapper = document.querySelector('.hole-wrapper');
+      for (let i = 1; i <= NumberHoles; i++) {
+        const hole = document.createElement('div');
+        hole.className = 'hole';
+        
+        // Genera una posizione casuale con tolleranza
+        const holeRandomPositionX = Math.floor(
+          Math.random() * (maxPositionX - visualHoleWidth - 2 * safeZoneWidth) +
+            safeZoneWidth
+        );
+
+      
+       // hole.style.left = holeRandomPositionX - tolerance + 'px';
+
+        // Imposta la larghezza visiva del buco
+        hole.style.width = visualHoleWidth + 'px';
+        
+        this.holes.push({
+          positionX: holeRandomPositionX ,
+        });
+         
+      
+        hole.style.left = holeRandomPositionX  - tolerance + 'px';
+
+        holeWrapper.appendChild(hole);
+      }
     },
 
     updateGame() {
@@ -118,7 +153,7 @@ export default {
     this.obstacles.forEach((obstacle) => {
       //console.log('Player:', this.playerPosition, this.playerWidth);
      // console.log('Obstacle:', obstacle.positionX, this.obstacleWidth);
-      const playerBottom = parseInt(getComputedStyle(document.getElementById('player')).bottom);
+      var playerBottom = parseInt(getComputedStyle(document.getElementById('player')).bottom);
 
       if (
         this.playerPosition + this.playerWidth >= obstacle.positionX &&
@@ -139,11 +174,35 @@ export default {
       
     }
 
+
+    //PROVA COLLISIONE BUCHI
+    let isCollidingHole = false;
+  this.holes.forEach((hole) => {
+    const playerBottom = parseInt(getComputedStyle(document.getElementById('player')).bottom);
+    
+     // Regola questo valore in base alle tue esigenze
+  if (
+    this.playerPosition + this.playerWidth >= hole.positionX &&
+    this.playerPosition <= hole.positionX + this.holeWidth &&
+    playerBottom <= this.holeHeigth 
+  ) {
+    isCollidingHole = true;
+  }
+  console.log(isCollidingHole)
+});
+
+  // Se c'è una collisione con un buco, aggiorna la posizione del giocatore solo se non sta saltando
+  if (isCollidingHole && !this.isJumping) {
+    // Imposta la nuova posizione verticale del giocatore (esempio: bottom: -170px)
+    const newBottomPosition = -170;
+    document.getElementById('player').style.bottom = newBottomPosition + 'px';
+  }
 },
 restart(){
   this.playerPosition=0;
   this.playerSpeed=10;
   this.obstacles=[];
+  this.holes=[];
   this.currentLevel=1;
   this.gameOver= false;
   this.jumpHeight=100;
@@ -171,6 +230,7 @@ nextLevel() {
   <div class="game-bg">
     <div id="player" :style="{ left: playerPosition + 'px' }"></div>
     <div class="obstacle-wrapper col-8 offset-2"></div>
+    <div class="hole-wrapper"></div>
     <div class="game-over" v-if="gameOver">
       <h1>Game-Over</h1>
     <button class="btn btn-warning" @click="restart()">restart</button>
@@ -212,5 +272,13 @@ nextLevel() {
   }
   body::-webkit-scrollbar {
   display: none;
+}
+.hole{
+  background-color: red;
+    position: absolute;
+    bottom:-180px;
+    
+    width: 80px;
+    height: 180px;
 }
 </style>
