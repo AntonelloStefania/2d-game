f865914e4727d0ea296a6047d2a43080effd452e
AntonelloStefania <antonelloste94@gmail.com>
2023-10-06 17:43:40 +0200

    sistemato ground

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,12 @@
 <script setup>
-import Game from './components/Game.vue'
+import Game from './components/Game.vue';
+import Ground from './components/Ground.vue';
 </script>
 
 <template>
 
   <Game />
+  <Ground/>
 </template>
 
 <style scoped>
